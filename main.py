940e3b760f5f61e97a827e47cd226823821c9751
Evan Gillespie <evan.gillespie1@gmail.com>
2014-07-06 23:15:17 -0600

    added a list of cities and get the sunset times for each

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.sunsetter import SunSetter
 
 def main():
 	s = SunSetter()
+	s.find_sunset_at_time()
 
 if __name__ == "__main__":
 	main()
--- src/sunsetter.py
+++ src/sunsetter.py
@@ -1,13 +1,21 @@
 from astral import Astral
+from .enums import CITY_NAMES
+from datetime import date, datetime, timedelta
+import pytz
 
 class SunSetter(object):
 	"""
 	"""
 	def __init__(self):
 		super(SunSetter, self).__init__()
-		self.prime()
+
+		self.a = Astral()
+		self.cities = dict()
+		self.date = datetime.now(pytz.UTC)	# date to find sunrise/sunset for
 		
-	def find_sunset_at_time(self, time):
+		self._prime()
+
+	def find_sunset_at_time(self, time=None):
 		"""
 		get the name of a major city where the sun is setting at a given time
 
@@ -15,10 +23,35 @@ class SunSetter(object):
 
 		:return str: city name or None
 		"""
-		pass
+		if not time:
+			time = datetime.now(pytz.UTC)
 
-	def prime(self):
+		for city, data in self.cities.iteritems():
+			delt = time-data['sunset']
+
+			print "%s: %s" % (city, delt.seconds)
+
+	def _prime(self):
 		"""
 		read all the important data into memory
 		"""
-		pass
+		self._get_cities()
+
+	def _get_cities(self):
+		for name, continent in CITY_NAMES.iteritems():	
+			for city in continent:
+				try:
+					c = self.a[city]
+					s = c.sun(date=self.date, local=False)
+
+					city_dict = {
+						# "city": c,
+						# "sun": s,
+						"sunset": s['sunset'],
+						"sunrise": s['sunrise']
+					}
+
+					self.cities[city] = city_dict
+
+				except KeyError:
+					print "NO - %s" % city
