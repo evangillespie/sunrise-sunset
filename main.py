a644ac93cd8527693aafc4b49a3dfefdbf55202e
Evan Gillespie <evan.gillespie1@gmail.com>
2014-07-06 18:28:36 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.sunsetter import SunSetter
+
+def main():
+	s = SunSetter()
+
+if __name__ == "__main__":
+	main()
---
+++ src/sunsetter.py
@@ -0,0 +1,24 @@
+from astral import Astral
+
+class SunSetter(object):
+	"""
+	"""
+	def __init__(self):
+		super(SunSetter, self).__init__()
+		self.prime()
+		
+	def find_sunset_at_time(self, time):
+		"""
+		get the name of a major city where the sun is setting at a given time
+
+		:param time: time of the sunset
+
+		:return str: city name or None
+		"""
+		pass
+
+	def prime(self):
+		"""
+		read all the important data into memory
+		"""
+		pass
