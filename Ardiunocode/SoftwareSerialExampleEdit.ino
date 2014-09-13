014314ac8964ec5066d5a06cf39c42e8754adab0
Lowell Smith <lowell bert@Macintosh.local>
2014-09-13 09:52:02 -0600

    added Ardiuino Code

---
+++ Ardiunocode/SoftwareSerialExampleEdit.ino
@@ -0,0 +1,85 @@
+/*
+  Software serial multple serial test
+ 
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+ 
+ The circuit: 
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+ 
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts, 
+ so only the following can be used for RX: 
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+ 
+ Not all pins on the Leonardo support change interrupts, 
+ so only the following can be used for RX: 
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+ 
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+ 
+ This example code is in the public domain.
+ 
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(4800);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  Serial.println("Send Message");
+
+  byte init = B10001000;
+  byte addr = 64;
+  byte code = 64;
+  byte clos = B10000001;
+ 
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(4800);
+  
+  mySerial.print(byte(2));
+  mySerial.print(byte(3));
+  
+  mySerial.print(init);
+  mySerial.print(addr);
+  mySerial.print(code);
+  mySerial.print(clos);
+  
+  mySerial.print(init);
+  mySerial.print(addr);
+  mySerial.print(byte(65));
+  mySerial.print(clos);
+
+  mySerial.print(init);
+  mySerial.print(addr);
+  mySerial.print(byte(66));
+  mySerial.print(clos);
+
+  mySerial.print(init);
+  mySerial.print(addr);
+  mySerial.print(byte(10));
+  mySerial.print(clos);
+  
+  mySerial.print(clos);
+
+}
+
+void loop() // run over and over
+{
+  if (mySerial.available())
+    Serial.write(mySerial.read());
+  if (Serial.available())
+    mySerial.write(Serial.read());
+}
+
